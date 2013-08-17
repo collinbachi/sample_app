7a006cee45d9430e057a91aa5f29a0b76fcb14e8
Daniel C. Bachi <collinbachi@gmail.com>
2013-08-17 16:20:46 -0400

    still filling layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,10 +4,10 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
+    #it "should have the h1 'Sample App'" do
+      #visit '/static_pages/home'
+      #page.should have_selector('h1', :text => 'Sample App')
+    #end
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
@@ -46,6 +46,20 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_selector('title',
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end

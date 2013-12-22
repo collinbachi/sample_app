00a6909d5128b1a1ab5dc9ef640b37cf1762e971
Daniel C. Bachi <collinbachi@gmail.com>
2013-12-22 13:40:46 -0500

    finish signin/signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
-  resources :Sessions, only: [:new, :create, :destroy]
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to =>  'static_pages#home'
   match '/signup',  to: 'users#new'            
